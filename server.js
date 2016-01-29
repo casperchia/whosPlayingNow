447f9c078941b31fe4049f5652cfc0c84e5379e7
cchia <cchia@objectconsulting.com.au>
2016-01-29 14:44:41 +1100

    Add basic input form to take in username

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var config = require('./config');
-
+var pgp = require('pg-promise')({
+    // Initialization Options
+});
+var db = pgp(config.dbConnection);
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
    res.render('index', {bodyText: 'Hello World!'})
+})
+
+app.get('/profile', function(req, res){
+   res.render('index', {bodyText: req.query.id})
 })
 
 var server = app.listen(8080, function () {

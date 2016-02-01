7051175b3b09a816226350e396f32f062371651a
cchia <cchia@objectconsulting.com.au>
2016-02-01 15:06:58 +1100

    Refactor code into small functions for easier reading

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var options = {
 }
 var Steam = require('steam-webapi');
 Steam.key = config.steamkey;
+var steam = new Steam();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,53 +29,7 @@ app.get('/', function (req, res) {
 app.get('/profile', function(req, res){
    Steam.ready(function(err){
       if(err) return console.error(err);
-
-      var steam = new Steam();
-
-      steam.resolveVanityURL({vanityurl: req.query.id}, function getGamesForUser(err, steamId){
-         if(steamId.success == 1){
-            steamId.include_appinfo = true;
-            steamId.include_played_free_games=true;
-            steamId.appids_filter="";
-            steam.getOwnedGames(steamId, function getNumOfPlayersForGames(err, gamesList){
-               for(var i in gamesList.games){
-                  var id = gamesList.games[i].appid;
-                  // console.log(id);
-                  // Get number of players for game
-                  options.path = currentPlayersPath + id;
-                  http.get(options, function processHttpResponse(res){
-                     var responses = [];
-                     res.on('data', function(chunk){
-                        responses.push(chunk);
-                     });
-                     res.on('end', function extractResponseData(){
-                        var gameData = JSON.parse(responses.join());
-                        if(gameData.response.result == 1){
-                           var url_parts = url.parse(res.req.path, true);
-                           var appid = url_parts.query.appid;
-                           // console.log(appid + " : " + gameData.response.player_count);
-                           db.one("SELECT * FROM games WHERE id = $1", [appid])
-                              .then(function(data){
-                                 console.log(data.id + " : " + data.name + " : " + gameData.response.player_count);
-                              })
-                              .catch(function(err){
-                                 console.log("ERROR:", err.message);
-                              })
-                           // console.log(res.req.path);
-                        }else{
-                           console.log("No results for " + res.req.path);
-                        }
-                     })
-                  }).on('error', function(e){
-                     console.error(e);
-                  });
-               }
-               console.log("====================================\n");
-            });
-         }else{
-            console.log("Unable to retrieve profile for: " + req.query.id);
-         }
-      })
+      steam.resolveVanityURL({vanityurl: req.query.id}, getGamesForUser)
    })
    res.render('index', {bodyText: req.query.id})
 })
@@ -92,5 +47,59 @@ function getGamesWithCurrentPlayers(err, gamesList){
       steamObj.getNumberOfCurrentPlayers(steamObj, function(err, playerCount){
          console.log(playerCount);
       })
+   }
+}
+
+function getGamesForUser(err, steamId){
+   if(steamId.success == 1){
+      steamId.include_appinfo = true;
+      steamId.include_played_free_games=true;
+      steamId.appids_filter="";
+      steam.getOwnedGames(steamId, getNumOfPlayersForGames);
+   }else{
+      console.log("Unable to retrieve profile for: " + req.query.id);
+   }
+}
+
+function getNumOfPlayersForGames(err, gamesList){
+   for(var i in gamesList.games){
+      var id = gamesList.games[i].appid;
+      // console.log(id);
+      // Get number of players for game
+      options.path = currentPlayersPath + id;
+      http.get(options, processHttpResponse)
+         .on('error', function(e){
+            console.error(e);
+         });
+   }
+   console.log("====================================\n");
+}
+
+function processHttpResponse(res){
+   var responses = [];
+   res.on('data', function(chunk){
+      responses.push(chunk);
+   });
+   res.on('end', function(){
+      return extractResponseData(responses, res);
+   });
+}
+
+function extractResponseData(responses, res){
+   var gameData = JSON.parse(responses.join());
+   if(gameData.response.result == 1){
+      var url_parts = url.parse(res.req.path, true);
+      var appid = url_parts.query.appid;
+      // console.log(appid + " : " + gameData.response.player_count);
+      db.one("SELECT * FROM games WHERE id = $1", [appid])
+         .then(function(data){
+            console.log(data.id + " : " + data.name + " : " + gameData.response.player_count);
+         })
+         .catch(function(err){
+            console.log("ERROR:", err.message);
+         })
+      // console.log(res.req.path);
+   }else{
+      console.log("No results for " + res.req.path);
    }
 }

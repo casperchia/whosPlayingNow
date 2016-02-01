19d42f2a0bb39ab08de8f36d6beaaec1b6001aa0
cchia <cchia@objectconsulting.com.au>
2016-02-02 10:04:06 +1100

    Server basic functionality now works. Can get results for users in a table. However it cannot run in parallel yet - multiple users using it will crash it

--- server.js
+++ server.js
@@ -18,21 +18,43 @@ var options = {
 var Steam = require('steam-webapi');
 Steam.key = config.steamkey;
 var steam = new Steam();
+var games = []
+var completedRequests = 0;
+var gamesCount;
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+var resultsRenderer;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-   res.render('index', {bodyText: 'Hello World!'})
+   res.render('index', {bodyText: 'Hello World!', games: []})
 })
 
 app.get('/profile', function(req, res){
+   games = [];
+   completedRequests = 0;
+   resultsRenderer = renderResults(req, res);
    Steam.ready(function(err){
       if(err) return console.error(err);
       steam.resolveVanityURL({vanityurl: req.query.id}, getGamesForUser)
    })
-   res.render('index', {bodyText: req.query.id})
 })
+
+function renderResults(req, res){
+   var req = req;
+   var res = res;
+
+   function complete(){
+      if(completedRequests == gamesCount){
+         res.render('index', {bodyText: req.query.id, games: games})
+      }
+   }
+   return {
+      complete:complete
+   }
+}
 
 var server = app.listen(8080, function () {
   var port = server.address().port
@@ -41,16 +63,18 @@ var server = app.listen(8080, function () {
 
 function getGamesForUser(err, steamId){
    if(steamId.success == 1){
+      console.log("Retrieved profile");
       steamId.include_appinfo = true;
       steamId.include_played_free_games=true;
       steamId.appids_filter="";
       steam.getOwnedGames(steamId, getNumOfPlayersForGames);
    }else{
-      console.log("Unable to retrieve profile for: " + req.query.id);
+      console.log("Unable to retrieve profile");
    }
 }
 
 function getNumOfPlayersForGames(err, gamesList){
+   gamesCount = gamesList.games.length;
    for(var i in gamesList.games){
       var id = gamesList.games[i].appid;
       options.path = currentPlayersPath + id;
@@ -77,9 +101,16 @@ function extractResponseData(responses, res){
    if(gameData.response.result == 1){
       var url_parts = url.parse(res.req.path, true);
       var appid = url_parts.query.appid;
+      var playerCount = gameData.response.player_count;
       db.one("SELECT * FROM games WHERE id = $1", [appid])
          .then(function(data){
-            console.log(data.id + " : " + data.name + " : " + gameData.response.player_count);
+            // console.log(data.id + " : " + data.name + " : " + playerCount);
+            var game = {};
+            game.id = data.id;
+            game.name = data.name;
+            game.playerCount = playerCount;
+            games.push(game);
+            // console.log(games);
          })
          .catch(function(err){
             console.log("ERROR:", err.message);
@@ -87,4 +118,6 @@ function extractResponseData(responses, res){
    }else{
       console.log("No results for " + res.req.path);
    }
+   completedRequests++;
+   resultsRenderer.complete();
 }
